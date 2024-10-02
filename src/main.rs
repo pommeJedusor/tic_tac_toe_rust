680956e685bc6703987d4b9d2cdbbce90ab5ba2a
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-10-02 08:32:57 +0200

    binary -> 2dArray

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,99 @@
 use std::io;
 
-const FULL_GRID: u32 = 0b11101110111;
+type Grid = [[u32; 3]; 3];
 
-fn get_moves(grid: u32) -> [bool; 9] {
+fn get_grid() -> [[u32; 3]; 3] {
+    return [[0; 3]; 3];
+}
+
+fn get_moves(grid: Grid) -> [bool; 9] {
     let mut result = [false; 9];
     for y in 0..3 {
         for x in 0..3 {
-            result[y * 3 + x] = grid & 1 << (y * 4 + x) == 0;
+            result[y * 3 + x] = grid[y][x] == 0;
         }
     }
+
     result
 }
 
-fn _show_grid(player_1: u32, player_2: u32) {
+fn _show_grid(grid: Grid) {
     for y in 0..3 {
         let mut row = String::new();
         for x in 0..3 {
-            let i = y * 4 + x;
-            row += if player_1 & 1 << i != 0 {
-                "1"
-            } else if player_2 & 1 << i != 0 {
-                "2"
-            } else {
-                "0"
-            };
+            row.push(char::from_digit(grid[y][x], 10).unwrap());
         }
         println!("{row}");
     }
 }
 
-fn make_move(grid: u32, r#move: usize) -> u32 {
-    let offset = r#move / 3;
-    grid | 1 << (r#move + offset)
+fn make_move(grid: &mut Grid, r#move: usize, player: u32) {
+    let y: usize = r#move / 3;
+    let x: usize = r#move % 3;
+    grid[y][x] = player;
 }
 
-fn is_winning(grid: u32) -> bool {
-    // horizontal
-    if grid & grid << 1 & grid << 2 != 0 {
-        return true;
-    }
-    // vertical
-    if grid & grid << 4 & grid << 8 != 0 {
-        return true;
+fn cancel_move(grid: &mut Grid, r#move: usize) {
+    let y: usize = r#move / 3;
+    let x: usize = r#move % 3;
+    grid[y][x] = 0;
+}
+
+fn is_winning(grid: Grid) -> u32 {
+    for i in 0..3 {
+        // horizontal
+        if grid[i][0] != 0 && grid[i][0] == grid[i][1] && grid[i][2] == grid[i][1] {
+            return grid[i][0];
+        }
+        // vertical
+        if grid[0][i] != 0 && grid[0][i] == grid[1][i] && grid[2][i] == grid[1][i] {
+            return grid[0][i];
+        }
     }
     // top left -> bottom right
-    if grid & grid << 5 & grid << 10 != 0 {
-        return true;
+    if grid[1][1] != 0 && grid[0][0] == grid[1][1] && grid[2][2] == grid[1][1] {
+        return grid[1][1];
     }
     // top right -> bottom left
-    if grid & grid << 3 & grid << 6 != 0 {
-        return true;
+    if grid[1][1] != 0 && grid[0][2] == grid[1][1] && grid[2][0] == grid[1][1] {
+        return grid[1][1];
     }
-    false
+
+    0
 }
 
-fn is_game_finished(grid: u32) -> bool {
-    grid ^ FULL_GRID == 0
+fn is_game_finished(grid: Grid) -> bool {
+    for y in 0..3 {
+        for x in 0..3 {
+            if grid[y][x] == 0 {
+                return false;
+            }
+        }
+    }
+
+    true
 }
 
 // return 42 when no move available
-fn get_best_move(player_1: u32, player_2: u32, depth: i8) -> (usize, i8) {
-    if is_winning(player_2) {
+fn get_best_move(grid: &mut Grid, depth: i8) -> (usize, i8) {
+    if is_winning(*grid) != 0 {
         return (42, depth - 9);
     }
-    if is_game_finished(player_1 | player_2) {
+    if is_game_finished(*grid) {
         return (42, 0);
     }
     let mut best_score = -10;
     let mut best_move = 0;
-    let moves = get_moves(player_1 | player_2);
+    let moves = get_moves(*grid);
     for i in 0..9 {
         if !moves[i] {
             continue;
         }
 
-        let (_, score) = get_best_move(player_2, make_move(player_1, i), depth + 1);
+        make_move(grid, i, u32::try_from((depth + 1) % 2 + 1).unwrap());
+        let (_, score) = get_best_move(grid, depth + 1);
         let score = -score;
+        cancel_move(grid, i);
 
         if score > best_score {
             best_score = score;
@@ -102,8 +120,8 @@ fn is_player_first() -> bool {
     }
 }
 
-fn get_player_move(player_1: u32, player_2: u32) -> usize {
-    let available_moves = get_moves(player_1 | player_2);
+fn get_player_move(grid: Grid) -> usize {
+    let available_moves = get_moves(grid);
     loop {
         // get input
         let mut result = String::new();
@@ -142,18 +160,17 @@ fn launch_game() {
     println!("-- Do you want to start? [y/n] --");
     let is_bot_first = !is_player_first();
     let mut turn = 1;
-    let mut player_1 = 0;
-    let mut player_2 = 0;
-    _show_grid(player_1, player_2);
+    let mut grid = get_grid();
+    _show_grid(grid);
     let result = loop {
         // check game status
-        if is_winning(player_1) && is_bot_first || is_winning(player_2) && !is_bot_first {
+        if is_winning(grid) == 1 && is_bot_first || is_winning(grid) == 2 && !is_bot_first {
             break "You lost";
         }
-        if is_winning(player_2) && is_bot_first || is_winning(player_1) && !is_bot_first {
+        if is_winning(grid) == 2 && is_bot_first || is_winning(grid) == 1 && !is_bot_first {
             break "You won";
         }
-        if is_game_finished(player_1 | player_2) {
+        if is_game_finished(grid) {
             break "Draw";
         }
 
@@ -162,22 +179,22 @@ fn launch_game() {
         // get move
         let r#move: usize;
         if is_bot_first && turn % 2 == 1 || !is_bot_first && turn % 2 == 0 {
-            (r#move, _) = get_best_move(player_1, player_2, turn);
+            (r#move, _) = get_best_move(&mut grid, turn);
             let str_move = r#move + 1;
             println!("I play {str_move}");
         } else {
             println!("Play a move");
-            r#move = get_player_move(player_1, player_2);
+            r#move = get_player_move(grid);
         }
 
         // make move
         if turn % 2 == 1 {
-            player_1 = make_move(player_1, r#move);
+            make_move(&mut grid, r#move, 1);
         } else {
-            player_2 = make_move(player_2, r#move);
+            make_move(&mut grid, r#move, 2);
         }
         turn += 1;
-        _show_grid(player_1, player_2);
+        _show_grid(grid);
     };
     println!("{result}");
 }
